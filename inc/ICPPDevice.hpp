82a2f28adbc86d5552d34dbfc45257f3f34409b0
technical <s.hatefinia@tosantechno.com>
2022-07-20 10:38:45 +0430

    First commit

--- inc/ICPPDevice.hpp
+++ inc/ICPPDevice.hpp
@@ -21,7 +21,6 @@ namespace __N_CPPDEVICE__
 	typedef struct _device_event_data
 	{
 		unsigned short int	uid;                            // event unique id
-		XFSVERSION          ver{ XFSVERSION::NXFS };        // xfs version
 		int			        rid;                            // request id
 		int			        msg;                            // message id
 		int			        res;                            // result
@@ -52,6 +51,8 @@ namespace __N_CPPDEVICE__
         virtual bool UnInitialize() noexcept = 0;
         virtual bool IsInitialize() const noexcept = 0;
 
+		virtual void RegisterCallbackFunction(DEVEVENTLAMBDA) noexcept = 0;
+
 		virtual bool Connect_sync() noexcept = 0;
 		virtual bool Connect_async() noexcept = 0;
 		virtual bool Disconnect_async() noexcept = 0;
@@ -60,15 +61,12 @@ namespace __N_CPPDEVICE__
 		virtual bool IsConnected() const noexcept = 0;
 		virtual bool IsConnecting() const noexcept = 0;
 
-		virtual std::string GetLogicName() const noexcept = 0;
-
-		virtual void RegisterCallbackFunction(DEVEVENTLAMBDA) noexcept = 0;
-
 		virtual bool LockDevice(long Timeout) noexcept = 0;
 		virtual bool UnLockDevice() noexcept = 0;
 		virtual bool CancelLock() noexcept = 0;
 
 		virtual std::string GetDeviceLastError() noexcept = 0;
+		virtual std::string GetLogicName() const noexcept = 0;
     };
 } // !__N_CPPDEVICE__
 
