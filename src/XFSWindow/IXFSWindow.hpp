5fc6a45e97a6f017cdb1a95e7c32e03226838bda
technical <s.hatefinia@tosantechno.com>
2022-08-17 16:21:12 +0430

    XFSWindow updated

---
+++ src/XFSWindow/IXFSWindow.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#ifndef __H_IXFSWINDOW__
+#define __H_IXFSWINDOW__
+
+#include <design/IObserver.hpp>
+
+#include <functional>
+#include <memory>
+#include <string>
+
+namespace __N_XFSWINDOW__
+{
+    typedef struct _xfs_window_event_context
+    {
+        unsigned int msg{ 0 };
+        int lparam{ 0 };
+    } CONTEXT, * LPCONTEXT;
+
+	using LAMBDA = __N_OBSERVER__::LAMBDA<CONTEXT>;
+
+    typedef struct _xfs_window_init
+    {
+		std::string window_class{ "XFS_WINDOW_CLASS" };
+		std::string window_name{ "XFS_WINDOW_NAME" };
+        LAMBDA lambda{};
+    } INIT, * LPINIT;
+
+	class IXFSWindow : public __N_OBSERVER__::IPublisher<CONTEXT>
+	{
+	private:
+	protected:
+	public:
+		IXFSWindow() = default;
+		IXFSWindow(const IXFSWindow&) = delete;
+		IXFSWindow(IXFSWindow&&) = delete;
+		IXFSWindow& operator = (const IXFSWindow&) = delete;
+		IXFSWindow& operator = (IXFSWindow&&) = delete;
+		virtual ~IXFSWindow() = default;
+
+        virtual bool Initialize(const INIT&) noexcept = 0;
+        virtual bool UnInitialize() noexcept = 0;
+        virtual bool IsInitialized() const noexcept = 0;
+
+		virtual int	 GetWindowHandle() const noexcept = 0;
+        virtual std::string GetLastError() const noexcept = 0;
+	};
+
+	std::shared_ptr<IXFSWindow> CreateXFSWindow() noexcept;
+} // !__N_XFSWINDOW__
+
+#endif // !__H_IXFSWINDOW__
---
+++ src/XFSWindow/Implementations/CXFSWindow.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#ifndef __H_CXFSWINDOW__
+#define __H_CXFSWINDOW__
+
+#include "../IXFSWindow.h"
+
+#include <ICPPMSGWND.hpp>
+
+#include <memory>
+
+namespace __N_XFSWINDOW__
+{
+	class CXFSWindow : public IXFSWindow
+	{
+	private:
+        INIT m_init{};
+		std::shared_ptr<__N_CPPMSGWND__::ICPPMSGWND> m_pMSGWND{ __N_CPPMSGWND__::CreateCPPMSGWND() };
+        
+        __N_CPPWNDMSG__::WND_LAMBDA m_lambda{ [](
+            const __N_CPPWNDMSG__::WND_HWND     hwnd, 
+            const __N_CPPWNDMSG__::WND_MSG      msg, 
+            const __N_CPPWNDMSG__::WND_WPARAM   wparam, 
+            const __N_CPPWNDMSG__::WND_LPARAM   lparam) {
+                __N_OBSERVER__::XFSWINDOWEVENTCONTEXT event_context{};
+                event_context.msg       = msg;
+                event_context.lparam    = lparam;
+                this->notify(event_context);
+                this->notify();
+        }};
+
+	protected:
+	public:
+		CXFSWindow() = default;
+		CXFSWindow(const CXFSWindow&) = delete;
+		CXFSWindow(CXFSWindow&&) = delete;
+		CXFSWindow& operator = (const CXFSWindow&) = delete;
+		CXFSWindow& operator = (CXFSWindow&&) = delete;
+		virtual ~CXFSWindow() = default;
+
+        bool Initialize(const INIT&) noexcept final;
+        bool UnInitialize() noexcept final;
+        bool IsInitialized() const noexcept final { return this->m_pMSGWND->IsInitialized(); }
+
+		int	 GetWindowHandle() const noexcept { return this->m_pMSGWND->GetHWND(); }
+        virtual std::string GetLastError() const noexcept final { return this->m_pMSGWND->getLastError(); }
+	};
+} // !__N_XFSWINDOW__
+
+#endif // !__H_CXFSWINDOW__
